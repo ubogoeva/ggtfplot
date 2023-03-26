16b77ac7100c2c5060c699e62d2875835230683d
Elena Ubogoeva <47380082+ubogoeva@users.noreply.github.com>
2023-03-26 13:36:08 +0600

    added function description and license

--- R/draw_TF_plot.R
+++ R/draw_TF_plot.R
@@ -1,5 +1,22 @@
+#' Build transcription factor on promoters plot
+#'
+#' @param df Dataset of three column: 1st is TF name, 2nd is coordinates of begin, 3d is coords of ending
+#' @param prom_length Promoter length to plot
+#' @param title Title of plot (usually gene name)
+#'
+#' @return ggplot of TFs binds in promoter
+#' @export
+#'
+#' @examples
+#' test_df <- data.frame(TF = c('TF1', 'TF2', 'TF3'),
+#'                   rel_beg = c(-1000, -700, -400),
+#'                   rel_end = c(-800, -450, -300))
+#' draw_TF_plot(test_df)
 draw_TF_plot <- function(df, prom_length = 1500, title = 'gene') {
+  if (ncol(df) > 3) warning('Dataset contain more than 3 column, used first three')
+  if (ncol(df) < 3) stop('Dataset contain less than three required columns')
   x_breaks_prom <- c(seq(-prom_length-500,400,300)[seq(-prom_length-500,400,300)!= 0])
+  colnames(df)[1:3] <- c('TF', 'rel_beg', 'rel_end')
   df |>
     dplyr::group_by(TF) |> # to group_by
     dplyr::mutate(height = dplyr::cur_group_id()) |> # to create column with same height for same TF
@@ -24,9 +41,11 @@ draw_TF_plot <- function(df, prom_length = 1500, title = 'gene') {
     ggplot2::ggtitle(title)+
     # draw arrow as TSS
     ggplot2::geom_segment(ggplot2::aes(x = 1, y = -0.5, xend = 1, yend = 1), color = 'black')+
-    ggplot2::geom_segment(ggplot2::aes(x = 1, y = 1, xend = 100, yend = 1), arrow = ggplot2::arrow(length = ggplot2::unit(0.02,units = "npc")),
-                          size = 0.8, color = 'black')+
-    ggplot2::geom_text(ggplot2::aes(20, 2.1, label = 'TSS'), color = 'black', size = 3)+
+    ggplot2::geom_segment(ggplot2::aes(x = 1, y = 1, xend = 100, yend = 1),
+                          arrow = ggplot2::arrow(length = ggplot2::unit(0.02, units = "npc")),
+                          linewidth = 0.8, color = 'black')+
+    ggplot2::geom_text(ggplot2::aes(20, 2.1, label = 'TSS'),
+                       color = 'black', size = 3)+
     ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, face = 'italic'),
                    axis.title.y=ggplot2::element_blank(),
                    axis.text.y=ggplot2::element_blank(),
@@ -35,5 +54,5 @@ draw_TF_plot <- function(df, prom_length = 1500, title = 'gene') {
   # plot_sample  <- renderPlot({  data$sample_plot })
 }
 # ein3 <- vroom::vroom('data/template.txt', delim = "\t", col_names = c('TF', 'rel_beg', 'rel_end'))
-# draw_TF_plot(ein3)
+# draw_TF_plot(ein3[,1:2])
 # x_breaks_prom <- reactive(c(seq(-input$prom_length-500,400,300)[seq(-input$prom_length-500,400,300)!= 0]))
